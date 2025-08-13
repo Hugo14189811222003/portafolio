72969281cb6f1fa2340ad4dd820882eaecf97b68
hugo14189811222003 <hugo.arcos14189811222003>
2025-08-13 16:45:03 -0600

    actualizacion de card giratorias

--- app/components/home/componentInfo.vue
+++ app/components/home/componentInfo.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="plastaNegra">
-    <div class="flip-card" onclick="this.classList.toggle('flipped')">
-      <div class="face flip-card-front boxUnique1">
+  <div class="flip-card">
+    <div class="flip-card-inner" onclick="this.classList.toggle('activo')">
+      <div class="flip-card-front">
         <h1 class="title">Desarrollador de Software</h1>
       </div>
-      <div class="flip-card-back boxUnique1">
+      <div class="flip-card-back">
         <p><strong>Back:</strong> Especializado en la creación de APIs eficientes y escalables.</p>
         <p><strong>Front:</strong> Desarrollo de interfaces web modernas y atractivas.</p>
       </div>
     </div>
-    <div class="flip-card" onclick="this.classList.toggle('flipped')">
-      <div class="face flip-card-front boxUnique2">
+    <div class="flip-card-inner" onclick="this.classList.toggle('activo')">
+      <div class="flip-card-front">
         <h1 class="title">Diseñador de interfaces</h1>
       </div>
-      <div class="flip-card-back boxUnique2">
-        <p><strong>UX/UI:</strong> Especializado en la creación de interfaces de usuario intuitivas y atractivas.</p>
+      <div class="flip-card-back">
+        <p><strong>UX/UI:</strong> Especializado en la creación de experiencias de usuario atractivas y funcionales en web y móvil.</p>
       </div>
     </div>
-    <div class="flip-card" onclick="this.classList.toggle('flipped')">
-      <div class="face flip-card-front boxUnique3">
+    <div class="flip-card-inner" onclick="this.classList.toggle('activo')">
+      <div class="flip-card-front">
         <h1 class="title">Diseñador grafico</h1>
       </div>
-      <div class="flip-card-back boxUnique3">
-        <p><strong>Branding:</strong> Especializado en la creación de marcas fuertes y memorables</p>
-        <p><strong>Textil:</strong> Diseño de camisas deportivas personalizadas.</p>
+      <div class="flip-card-back">
+        <p><strong>Branding:</strong> Especializado en la creación de identidades visuales atractivas y coherentes.</p>
+        <p><strong>Textil:</strong> Diseño de camisas deportivas para imprentas de sublimación.</p>
       </div>
     </div>
   </div>
