6f20a5b48b6351d45feee89788ceac87975e3f61
hugo14189811222003 <hugo.arcos14189811222003>
2025-11-09 10:43:52 -0600

    actualizacion de nuxt link

--- app/components/home/componentContact.vue
+++ app/components/home/componentContact.vue
@@ -2,7 +2,7 @@
     <div class="contentGradient">
         <h1>¿Listo para trabajar juntos?</h1>
         <h2>Estoy disponible para proyectos freelance y colaboraciones</h2>
-        <button><span>Empecemos a hablar</span></button>
+        <nuxt-link to="/contacto"><button><span>Empecemos a hablar</span></button></nuxt-link>
     </div>
 </template>
 
